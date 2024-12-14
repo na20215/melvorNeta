50673af3205b13891a495f502969770d38928eca
na20215 <na20215@bristol.ac.uk>
2024-12-14 21:43:27 +0800

    Deploy to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/meitaiwaimaiNeta/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/melvorNeta/' : '/'
 })
